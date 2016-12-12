fd2df51266ed523fe96b866489006bc58be9d576
sofili <slee8@L-SNVH0LCG8W-M.local>
2016-12-12 14:45:08 -0800

    added secret buttons

--- Tripper/Tripper/HomeViewController.swift
+++ Tripper/Tripper/HomeViewController.swift
@@ -162,22 +162,34 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
     }
 // MARK - Calendar
     
-    @IBAction func secretEventTap() {
-
-        
+    @IBAction func secretSFEventTap() {
         // create a corresponding local notification
-        print("user taps secret event btn")
+        print("user taps SF secret event btn")
         let city = "San Francisco"
         let notification = UILocalNotification()
         notification.alertBody = "Traveling to \(city), would you be interested checking out movies that were shot at there? "
         notification.alertAction = "Yes, take me to Tripper"
         notification.fireDate = NSDate(timeIntervalSinceNow: +4) as Date
         notification.soundName = UILocalNotificationDefaultSoundName // play default sound
-        notification.userInfo = ["action": "goToTripper"]
+        notification.userInfo = ["action": "SF"]
         notification.category = "tripperCategory"
         UIApplication.shared.scheduleLocalNotification(notification)
-    
     }
+    
+    @IBAction func secretJPEventTap() {
+        // create a corresponding local notification
+        print("user taps JP secret event btn")
+        let city = "San Francisco"
+        let notification = UILocalNotification()
+        notification.alertBody = "Traveling to \(city), would you be interested checking out movies that were shot at there? "
+        notification.alertAction = "Yes, take me to Tripper"
+        notification.fireDate = NSDate(timeIntervalSinceNow: +4) as Date
+        notification.soundName = UILocalNotificationDefaultSoundName // play default sound
+        notification.userInfo = ["action": "JP"]
+        notification.category = "tripperCategory"
+        UIApplication.shared.scheduleLocalNotification(notification)
+    }
+    
     func checkCalendarAuthorizationStatus() {
         let status = EKEventStore.authorizationStatus(for: EKEntityType.event)
         
